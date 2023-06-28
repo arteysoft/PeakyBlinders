70fdb98f54de187a5fa769346a5ade034b9c96d8
andy <arteysoft@gmail.com>
2023-06-28 11:33:08 -0300

    ejercicio

--- clase03/src/index.ts
+++ clase03/src/index.ts
@@ -1,4 +1,21 @@
-let saludo:string = 'hola'
+import { Usuario } from "./interfaces/Usuario"
+
+setTimeout(() => {}, 1000000)
+
+let saludo:string = 'hola que tal'
 
 console.log(saludo)
 
+let geremias:Usuario = {
+    id : 'id',
+    firstName: '1',
+    lastName: '2',
+    city: '3',
+    streetName: '4',
+    country: '6',
+    accountName: '6',
+    account: '6',
+    amount: 612
+}
+
+console.log(geremias)
