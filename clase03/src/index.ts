d27124ecf4f63e9ea2ea67bb52f9c5e7edf2f3ec
andy <arteysoft@gmail.com>
2023-06-28 12:01:10 -0300

    clase #3

--- clase03/src/index.ts
+++ clase03/src/index.ts
@@ -1,21 +1,21 @@
-import { Usuario } from "./interfaces/Usuario"
+import { Usuario } from './interfaces/Usuario'
+import genUsuario from './lib/genusuario'
 
-setTimeout(() => {}, 1000000)
+// setTimeout(() => {}, 1000000)
 
-let saludo:string = 'hola que tal'
+let z = genUsuario()
+z = genUsuario()
+z = genUsuario()
+z = genUsuario()
 
-console.log(saludo)
+console.log(z)
 
-let geremias:Usuario = {
-    id : 'id',
-    firstName: '1',
-    lastName: '2',
-    city: '3',
-    streetName: '4',
-    country: '6',
-    accountName: '6',
-    account: '6',
-    amount: 612
-}
+/*
+Tarea:
 
-console.log(geremias)
+facil: Generar un archivo en formato json con un usuario puntual
+dificil: Generar 10/10000 archivos con diferentes usuarios
+
+los archivos se generan en /var/peakyblinders/f7a4d617-99cc-470e-8ef8-dd6323854e3c.json
+
+*/
