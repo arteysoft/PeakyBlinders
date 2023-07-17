9126e2d0259143b5fbe31455571ebbac446bd068
andy <arteysoft@gmail.com>
2023-07-17 11:38:00 -0300

    ejercicio 2

--- clase08/src/appserver/server.ts
+++ clase08/src/appserver/server.ts
@@ -1,13 +1,29 @@
 import express from 'express'
+import genUsuario from '../lib/genusuario'
 
 export default () => {
 
     let app = express()
 
+    app.use(express.json())
+
+    app.get('/alumno/inventar', (request, response) => {
+        response
+            .status(200)
+            .send(genUsuario())
+        
+    })
+
     app.get('/alumno', (request, response) => {
         response
             .status(200)
             .send('<h1>alumno roberto</h1>')
+    })
+
+    app.post('/alumno', (request, response) => {
+        // insertar en mongodb
+        console.log(request.body)
+        response.status(201).send()
     })
 
     app.listen(3000)
