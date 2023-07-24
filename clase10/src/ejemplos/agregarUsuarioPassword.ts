b848ba6f0795b29e2d534644d42441cc26f9942d
andy <arteysoft@gmail.com>
2023-07-24 11:31:00 -0300

    ejercicio 1

---
+++ clase10/src/ejemplos/agregarUsuarioPassword.ts
@@ -0,0 +1,16 @@
+export default () => {
+    // Hay que agregar un usuario de la siguiente form
+
+    /*
+
+    coleccion se llama usuarios
+
+    id UUID
+    nombreUsuario: 
+    passwordEncriptada:
+    salt: ya vamos a ver que es, le ponen holamundo
+
+    van a agregar su nombre, y sunombre carlos1234
+
+    */
+}
